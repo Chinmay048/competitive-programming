c859cca3ce18109ea2a71d297b5af30c592e4357
Chinmay Soni <chinmaysoni227@gmail.com>
2026-01-05 14:51:19 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- leetcode/problems/missing_number/solution.cpp
+++ leetcode/problems/missing_number/solution.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0,j=0;i<=nums.size(),j<nums.size();i++,j++){
-            if(nums[j]!=i)
-                return i;
-        }
-        return nums.size();
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+            sum+=nums[i];
+        return ((nums.size()*(nums.size()+1))/2)-sum;
     }
 };
