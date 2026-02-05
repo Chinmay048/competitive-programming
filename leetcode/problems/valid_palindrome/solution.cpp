1ecf3817aca4dfea41cfe0c5073e077a9564f5e1
Chinmay Soni <chinmaysoni227@gmail.com>
2026-02-05 19:52:36 +0000

    Sync LeetCode submission - Valid Palindrome (cpp)

--- leetcode/problems/valid_palindrome/solution.cpp
+++ leetcode/problems/valid_palindrome/solution.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string ch;
-    for(int i=0;i<s.size();i++){
-        if(s[i]>=48&&s[i]<=57)
-            ch+=s[i];
-        else if(s[i]>=65&&s[i]<=90)
-            ch+=tolower(s[i]);
-        else if(s[i]>=97&&s[i]<=122)
-            ch+=s[i];
-    }
-    for(int i=0,j=ch.size()-1;i<ch.size(),j>=0;i++,j--) {
-        if(ch[i]!=ch[j])
-            return 0;
-    }
-    return 1;
+        int left = 0,right = s.length()-1;
+        while(left<right){
+            if(!isalnum(s[left])){
+                left++;
+                continue;
+            }else if(!isalnum(s[right])){
+                right -- ;
+                continue;
+            }else if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            left++,right--;
+        }
+        return true;
     }
 };
