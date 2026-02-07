6fcf1dd78d7296c2d05beefbbc4e02bed02b027b
Chinmay Soni <chinmaysoni227@gmail.com>
2026-02-07 06:12:26 +0000

    Sync LeetCode submission - Search Insert Position (cpp)

--- leetcode/problems/search_insert_position/solution.cpp
+++ leetcode/problems/search_insert_position/solution.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if(nums[nums.size()-1]<target)
-            return nums.size();
-        else if(nums[0]>target)
-            return 0;
-        else {
-            int left=0,right=nums.size()-1;
-            while(left<=right){
-                int mid=left+(right-left)/2;
-                if(nums[mid]==target)
-                    return mid;
-                else if(nums[mid]>target)
-                    right=mid-1;
-                else 
-                    left=mid+1;
-            }
-            if(left>right)  {
-                if(nums[right]>target)
-                    return right-1;
-                else 
-                    return right+1;}
-
-        }
-        return -1;
+        int left=0,right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                right=mid-1;
+            else 
+                left=mid+1;
+        } 
+        return left;
     }
 };
