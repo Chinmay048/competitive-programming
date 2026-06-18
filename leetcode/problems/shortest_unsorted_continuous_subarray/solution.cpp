61234a5804f988bc239f5de7ec210ba9368ab220
Chinmay Soni <chinmaysoni227@gmail.com>
2026-06-18 15:02:41 +0000

    Sync LeetCode submission - Shortest Unsorted Continuous Subarray (cpp)

---
+++ leetcode/problems/shortest_unsorted_continuous_subarray/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        stack<int> st;
+        int l = nums.size(); 
+        int r = 0;           
+        for (int i = 0; i < nums.size(); i++) {
+            while (!st.empty() && nums[st.top()] > nums[i]) {
+                l = min(l, st.top()); 
+                st.pop();
+            }
+            st.push(i);
+        }
+       while (!st.empty()) {
+            st.pop();
+        }
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] < nums[i]) {
+                r = max(r, st.top()); 
+                st.pop();
+            }
+            st.push(i);
+        }
+        return (r > l) ? (r - l + 1) : 0;
+    }
+}; 
