f08d95f78ca6e01b8fdc923dd5810099affea06a
Chinmay Soni <chinmaysoni227@gmail.com>
2026-07-28 10:09:29 +0000

    Sync LeetCode submission - Smallest Palindromic Rearrangement I (cpp)

---
+++ leetcode/problems/smallest_palindromic_rearrangement_i/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int count[26] = {0};
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        string left = "", middle = "";
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                left += string(count[i] / 2, 'a' + i);
+                if (count[i] % 2 != 0) {
+                    middle = string(1, 'a' + i);
+                }
+            }
+        }
+        string right = left;
+        reverse(right.begin(), right.end());
+        return left + middle + right;
+    }
+};
